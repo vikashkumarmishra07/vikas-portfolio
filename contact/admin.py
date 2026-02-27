89a3520ef13da7e393eaa14dbbb126959a8135e6
Vikash Kumar Mishra <vikas771734@gmail.com>
2026-02-27 14:07:36 +0530

    Convert to static site for Render

--- contact/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Contact
-
-
-@admin.register(Contact)
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'subject','message', 'created_at')
-    search_fields = ('name', 'email', 'subject')
-    list_filter = ('created_at',)
